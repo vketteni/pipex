f6f7b6331cf1797b7dfcc305492368f7157c1882
Vincent Ketteniss <vketteni@c4b2c1.42berlin.de>
2024-02-15 19:04:38 +0100

    Feb 15 24

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: vketteni <vketteni@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:20:39 by vketteni          #+#    #+#             */
-/*   Updated: 2024/02/07 21:38:56 by vketteni         ###   ########.fr       */
+/*   Updated: 2024/02/15 18:17:19 by vketteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	execute_command(char *cmd, char *envp[])
 	if (execve(path, cmd_split, envp) == -1)
 	{
 		free(path);
-		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putstr_fd("pipex: execve", 2);
 		ft_putendl_fd(cmd_split[0], 2);
 		free_string_arr(cmd_split);
 		exit(-1);
@@ -42,17 +42,20 @@ void	parent_process(int pipe_fd[2], char *argv[], char *envp[])
 {
 	int	fd;
 
-	if (access(argv[1], F_OK) == -1)
-		msg_error("Infile missing=-");
-	if (access(argv[1], R_OK) == -1)
-		msg_error("Write access denied.");
-	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-		exit(-1);
-	dup2(fd, STDOUT_FILENO);
-	dup2(pipe_fd[0], STDIN_FILENO);
-	close(pipe_fd[1]);
-	execute_command(argv[3], envp);
+	if (access(argv[4], F_OK) == -1)
+		perror("pipex");
+	else if ( access(argv[4], R_OK) == -1)
+		perror("pipex");
+	else
+	{
+		fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd == -1)
+			exit(-1);
+		dup2(fd, STDOUT_FILENO);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[1]);
+		execute_command(argv[3], envp);
+	}
 }
 
 void	child_process(int pipe_fd[2], char *argv[], char *envp[])
@@ -60,16 +63,19 @@ void	child_process(int pipe_fd[2], char *argv[], char *envp[])
 	int	fd;
 
 	if (access(argv[1], F_OK) == -1)
-		msg_error("Infile missing");
-	if (access(argv[1], R_OK) == -1)
-		msg_error("Read access denied.");
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		exit(-1);
-	dup2(fd, STDIN_FILENO);
-	dup2(pipe_fd[1], STDOUT_FILENO);
-	close(pipe_fd[0]);
-	execute_command(argv[2], envp);
+		perror("pipex");
+	else if (access(argv[1], R_OK) == -1)
+		perror("pipex");
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			exit(-1);
+		dup2(fd, STDIN_FILENO);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[0]);
+		execute_command(argv[2], envp);
+	}
 }
 
 int	main(int argc, char *argv[], char *envp[])
